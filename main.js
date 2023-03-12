de9490b132b56a894fa970fe283e944f6a7f9678
Robin Reyes <robinreyes.code@gmail.com>
2023-03-12 09:50:52 -0700

    first push of day

--- main.js
+++ main.js
@@ -78,6 +78,16 @@ const spinMachine = () => {
     }
   }
   const reels = [[], [], []];
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+  return reels;
 };
 
 console.log(spinMachine());
